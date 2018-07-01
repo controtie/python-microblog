455a090895e74e32a3d4a95051db19ffd798c01a
controtie <dylan.ad.tran@gmail.com>
2018-07-01 13:31:56 +0900

    add seen display & update logic

--- app/models.py
+++ app/models.py
@@ -8,7 +8,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)

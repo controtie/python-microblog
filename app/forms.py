39b2c1cad85584c63f2a7b8bce8dbe2316b47805
controtie <dylan.ad.tran@gmail.com>
2018-07-01 15:28:59 +0900

    add error handling and log files

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -28,3 +28,18 @@ class RegistrationForm(FlaskForm):
         existing_user = User.query.filter_by(email=email.data).first()
         if existing_user is not None:
             raise ValidationError('Please use a different email.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            existing_user = User.query.filter_by(username=username.data).first()
+            if existing_user is not None:
+                raise ValidationError('That username is already taken.')

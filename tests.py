92d735ba7fa5a4daecd797a3163a7224a072d976
controtie <dylan.ad.tran@gmail.com>
2018-07-07 17:54:13 +0900

    add unit tests for user model

---
+++ tests.py
@@ -0,0 +1,85 @@
+from datetime import datetime, timedelta
+import unittest
+from app import app, db
+from app.models import User, Post
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_password_hashing(self):
+        u = User(username='susan')
+        u.set_password('cat')
+        self.assertFalse(u.check_password('dog'))
+        self.assertTrue(u.check_password('cat'))
+
+    def test_avatar(self):
+        u = User(username='john', email='john@example.com')
+        self.assertEqual(u.avatar(128), ('https://source.unsplash.com/random/128x128'))
+
+    def test_follow(self):
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        self.assertEqual(u1.followed.all(), [])
+        self.assertEqual(u2.followed.all(), [])
+
+        u1.follow(u2)
+        db.session.commit()
+        self.assertTrue(u1.is_following(u2))
+        self.assertEqual(u1.followed.count(), 1)
+        self.assertEqual(u1.followed.first().username, 'susan')
+        self.assertEqual(u1.followers.count(), 0)
+        self.assertEqual(u2.followers.count(), 1)
+        self.assertEqual(u2.followers.first().username, 'john')
+
+        u1.unfollow(u2)
+        db.session.commit()
+        self.assertFalse(u1.is_following(u2))
+        self.assertEqual(u1.followed.count(), 0)
+        self.assertEqual(u2.followers.count(), 0)
+
+    def test_follow_posts(self):
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        u3 = User(username='mary', email='mary@example.com')
+        u4 = User(username='david', email='david@example.com')
+        db.session.add_all([u1, u2, u3, u4])
+
+        # testing backrefs... they're weird
+        p8 = Post(body='u1 post', user_id=u1.id)
+        p9 = Post(body='u1 post', author=u1)
+        self.assertEqual(p8.user_id, p9.user_id)
+        self.assertEqual(p9.author.username, 'john')
+        self.assertNotEqual(p8.author, p9.author)
+
+        p1 = Post(body='u1 post', author=u1)
+        p2 = Post(body='u2 post', author=u2)
+        p3 = Post(body='u3 post', author=u3)
+        p4 = Post(body='u4 post', author=u4)
+        db.session.add_all([p1, p2, p3, p4])
+        db.session.commit()
+
+        # setup followers
+        u1.follow(u2)
+        u1.follow(u4)
+        u2.follow(u3)
+        u3.follow(u4)
+        db.session.commit()
+
+        f1 = u1.followed_posts().all()
+        f2 = u2.followed_posts().all()
+        f3 = u3.followed_posts().all()
+        f4 = u4.followed_posts().all()
+        self.assertEqual(f1, [p2, p4, p1])
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)

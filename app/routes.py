092ff5b8628c5d37e90b15df2c7f912c6c9d590b
controtie <dylan.ad.tran@gmail.com>
2018-07-14 17:53:14 +0900

    add user post form, follow users route

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,25 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, UserPostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'Duke'},
-            'body': 'What a beautiful day in Tokyo!'
-        },
-        {
-            'author': {'username': 'Derek'},
-            'body': 'Last Disneyland Trip of the year!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = UserPostForm()
+    if form.validate_on_submit():
+        new_post = Post(body=form.body.data, author=current_user)
+        db.session.add(new_post)
+        db.session.commit()
+        flash('Your post has been published.')
+        return redirect(url_for('index'))
+
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home', posts=posts, form=form)
 
 @app.before_request
 def before_request():
@@ -86,10 +85,7 @@ def about():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        { "author": user, "body": "test Post #1" },
-        { "author": user, "body": "test Post #2" }
-    ]
+    posts = Post.query.filter_by(author=user)
     return render_template('user.html', user=user, posts=posts);
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -107,3 +103,39 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} can not be found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You can not follow yourself!')
+        return redirect(url_for('index'))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are now following {}'.format(username))
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} can not be found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You can not unfollow yourself!')
+        return redirect(url_for('index'))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are no longer following {}'.format(username))
+    return redirect(url_for('user', username=username))
+
+@app.route('/explore')
+def explore():
+    all_posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template(url_for('explore', posts=all_posts))
+
